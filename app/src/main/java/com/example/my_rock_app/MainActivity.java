d461341ab545fa3ff9348a3c790556b295243622
ghofrane <kamounghofrane5@gmail.com>
2023-06-16 15:01:01 +0200

    blalalalal

--- app/src/main/java/com/example/my_rock_app/MainActivity.java
+++ app/src/main/java/com/example/my_rock_app/MainActivity.java
@@ -11,4 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onItemClicked(Note note) {
+        lastClickedNote = note;
+        //TODO: For the assignment you can add the code for opening the AddNoteActivity
+        // for an existing Note - and handling its update - here
+    }
+
+
 }
