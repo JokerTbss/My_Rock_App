561231657745fe3cd3d8203282526a4465d7227e
ghofrane <kamounghofrane5@gmail.com>
2023-06-21 15:27:02 +0200

    baseline_arrow_back_24

--- app/src/main/java/com/example/my_rock_app/MyCollection.java
+++ app/src/main/java/com/example/my_rock_app/MyCollection.java
@@ -3,10 +3,12 @@ package com.example.my_rock_app;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,8 +60,16 @@ public class MyCollection extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_my_collection, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_collection, container, false);
+        Button back= view.findViewById(R.id.back_icon);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_myCollection_to_main_screen);
+            }
+        });
+        return view;
 
     }
 }
