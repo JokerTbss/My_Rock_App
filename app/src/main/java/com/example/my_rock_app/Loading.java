bdd5b6b649c1464dbb645f1e88a474aa1ec1301b
ghofrane <kamounghofrane5@gmail.com>
2023-06-25 16:25:31 +0200

    analyse screen

---
+++ app/src/main/java/com/example/my_rock_app/Loading.java
@@ -0,0 +1,29 @@
+package com.example.my_rock_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //reminder :  change main_screen with the analyse screen (that have information)
+                Intent i = new Intent(Loading.this,main_screen.class);
+                startActivity(i);
+
+                finish();
+
+            }
+        },1000);
+    }
+}
