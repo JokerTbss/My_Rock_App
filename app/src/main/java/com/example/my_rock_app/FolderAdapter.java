8e3da3b82b2efda40ad89f47167305757bc28db2
JokerTbss <99118408+JokerTbss@users.noreply.github.com>
2023-07-05 16:20:18 +0200

    Ghofs implementation of the folder system.

--- app/src/main/java/com/example/my_rock_app/FolderAdapter.java
+++ app/src/main/java/com/example/my_rock_app/FolderAdapter.java
@@ -1,47 +1,125 @@
 package com.example.my_rock_app;
 
-import android.content.Context;
+import static java.security.AccessController.getContext;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.util.List;
 
-/*public class FolderAdapter extends RecyclerView.Adapter<FolderAdapter.ViewHolder> {
+public class FolderAdapter extends RecyclerView.Adapter<FolderAdapter.FolderViewHolder> {
+    private List<String> folderList;
+    Activity activity;
+    NavController navController;
 
-    Context context;
-    ArrayList<Object>arrFolder;
-    FolderAdapter(Context context, ArrayList<Object> arrFolder){
-        this.context= context;
-        this.arrFolder=arrFolder;}
-    @NonNull
-    @Override
-    public FolderAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.listview,parent,false);
-        return new RecyclerView.ViewHolder(view);
+
+    public FolderAdapter(List<String> folderList, Activity activity, NavController navController) {
+        this.folderList = folderList;
+        this.activity = activity;
+        this.navController = navController;
     }
 
-
+    @NonNull
+    @Override
+    public FolderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_folder, parent, false);
+        return new FolderViewHolder(view);
+    }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-        ViewModel model= (ViewModel) arrFolder.get(position);
-        holder.img.setImage
-        holder.folder_name.setText(model.name);
-
-        holder.folder_name.
-
+    public void onBindViewHolder(@NonNull FolderViewHolder holder, int position) {
+        String folderName = folderList.get(position);
+        holder.folderNameTextView.setText(folderName);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return folderList.size();
     }
 
-    public class ViewHolder {
+    class FolderViewHolder extends RecyclerView.ViewHolder {
+        TextView folderNameTextView;
+        CardView card;
+
+        FolderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            folderNameTextView = itemView.findViewById(R.id.folderNameTextView);
+            card = itemView.findViewById(R.id.item_card);
+            card.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    showDeletePrompt(folderNameTextView.getText().toString());
+                    return false;
+                }
+            });
+            card.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("folderName",folderNameTextView.getText().toString());
+                    navController.navigate(R.id.action_myCollection_to_picturesFragment, bundle);
+                }
+            });
+        }
     }
-}*/
+
+    private  void showDeletePrompt(String folderName) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+
+        alert.setTitle("Delete Folder");
+        alert.setMessage("Are you sure you want to delete the folder '" + folderName + "'?");
+
+        alert.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Delete the folder
+                boolean isDeleted = deleteFolder(folderName);
+                if (isDeleted) {
+                    folderList.remove(folderName);
+                    FolderAdapter.this.notifyDataSetChanged();
+                } else {
+                    // Failed to delete the folder
+                }
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
+    }
+
+    private boolean deleteFolder(String folderName) {
+        File storageDirectory = activity.getFilesDir();
+        File folder = new File(storageDirectory, folderName);
+
+        if (folder.exists() && folder.isDirectory()) {
+            // Delete the folder and its contents
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    file.delete();
+                }
+            }
+            return folder.delete();
+        }
+        return false;
+    }
+}
+
--- app/src/main/java/com/example/my_rock_app/MyCollection.java
+++ app/src/main/java/com/example/my_rock_app/MyCollection.java
@@ -1,13 +1,30 @@
 package com.example.my_rock_app;
 
+import static com.example.my_rock_app.R.id.list_folder;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +32,9 @@ import androidx.navigation.Navigation;
  * create an instance of this fragment.
  */
 public class MyCollection extends Fragment {
+    private ListView listView;
+
+    Button addButton ;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +44,10 @@ public class MyCollection extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private List<String> folderList;
+    private RecyclerView recyclerView;
+    private FolderAdapter adapter;
+
 
     public MyCollection() {
         // Required empty public constructor
@@ -55,12 +79,18 @@ public class MyCollection extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         View view= inflater.inflate(R.layout.fragment_my_collection, container, false);
+
+        recyclerView = view.findViewById(list_folder);
+
         // Inflate the layout for this fragment
         Button back= view.findViewById(R.id.back_icon);
         back.setOnClickListener(new View.OnClickListener() {
@@ -73,10 +103,77 @@ public class MyCollection extends Fragment {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_myCollection_to_createFolder);
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+
+                alert.setTitle("Create New Folder");
+                alert.setMessage("Enter the name of the folder");
+
+                final EditText input = new EditText(getContext());
+                alert.setView(input);
+
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String value = input.getText().toString();
+                        folderList.add(value);
+                        adapter.notifyDataSetChanged();
+
+                        // Create the new folder in the storage location
+                        File storageDirectory = getActivity().getFilesDir();
+                        File newFolder = new File(storageDirectory, value);
+                        if (!newFolder.exists()) {
+                            boolean isCreated = newFolder.mkdir();
+                            if (isCreated) {
+                                // Folder created successfully
+                            } else {
+                                // Failed to create folder
+                            }
+                        } else {
+                            // Folder with the same name already exists
+                        }
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Canceled.
+                    }
+                });
+
+                alert.show();
             }
         });
         return view;
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Retrieve the list of folders
+        folderList = getFolderListFromStorage();
+
+        NavController navController = Navigation.findNavController(requireView());
+        // Create and set up the RecyclerView adapter
+        adapter = new FolderAdapter(folderList, getActivity(), navController);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    private List<String> getFolderListFromStorage() {
+        List<String> folderList = new ArrayList<>();
+
+        // Retrieve the list of folder names from the storage location
+        File storageDirectory = getActivity().getFilesDir();
+        File[] files = storageDirectory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    folderList.add(file.getName());
+                }
+            }
+        }
+
+        return folderList;
     }
 }
